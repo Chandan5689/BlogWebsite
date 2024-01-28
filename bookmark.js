2ac37c71a21fd23da82c97846022ed01a76dc744
OikawaToru1 <aniketadhikari2@gmail.com>
2024-01-28 10:26:19 +0545

    fixed bookmark error

--- bookmark.js
+++ bookmark.js
@@ -1,5 +1,21 @@
-const bookmarkBefore = document.querySelector(".save-icon .bookmark");
+const bookmarked = document.querySelectorAll(".bookmark");
+const newtip = document.querySelectorAll(".tooltip");
 
-bookmarkBefore.addEventListener("click",()=>{
-    console.log("bookmarked");
+
+bookmarked.forEach((element, index)=>{
+    element.addEventListener('click', ()=>{
+        if (!element.classList.contains('bookmarked')){
+            element.classList.add('bookmarked')
+            newtip[index].innerHTML = "Bookmarked"
+            bookmarked[index].src = "bookmark.png";
+        } else {
+            element.classList.remove('bookmarked')
+            newtip[index].innerHTML = "Bookmark"
+            bookmarked[index].src= "cass-icons/icons8-save-32.png";
+        }
+    })
 })
+
+
+
+
